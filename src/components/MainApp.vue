2fcb0ce9f490ee0ac9a2e9b546be81bc060ceb5e
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-03-22 12:02:26 +0100

    [New File] Created new js file - used it like a database for my array of products

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -1,4 +1,5 @@
 <script>
+import {products} from "../data.js"
 import CardApp from "../components/ProductCard.vue";
 export default {
     name: "AppMain",
@@ -7,57 +8,7 @@ export default {
     },
     data() {
         return {
-            myCards: [
-                {
-                    mainImg: "/public/img/1.webp",
-                    hoverImg: "/public/img/1b.webp",
-                    brand: "Levi's",
-                    description: "RELAXED FIT TEE UNISEX",
-                    priceNow: "14,99€ ",
-                    priceOld: "69,99€ ",
-                },
-                {
-                    mainImg: "/public/img/2.webp",
-                    hoverImg: "/public/img/2b.webp",
-                    brand: "Zara",
-                    description: "SLIM FIT",
-                    priceNow: "14,99€ ",
-                    priceOld: "19,99€ ",
-                },
-                {
-                    mainImg: "/public/img/3.webp",
-                    hoverImg: "/public/img/3b.webp",
-                    brand: "Dolce & Gabbana",
-                    description: "NORMAL FIT",
-                    priceNow: "14,99€ ",
-                    priceOld: "22,99€ ",
-                },
-                {
-                    mainImg: "/public/img/4.webp",
-                    hoverImg: "/public/img/4b.webp",
-                    brand: "Hugo Boss",
-                    description: "NORMAL FIT",
-                    priceNow: "14,99€ ",
-                    priceOld: "19,99€ ",
-                },
-                {
-                    mainImg: "/public/img/5.webp",
-                    hoverImg: "/public/img/5b.webp",
-                    brand: "OVS",
-                    description: "RELAXED FIT TEE UNISEX",
-                    priceNow: "14,99€ ",
-                    priceOld: "59,99€ ",
-                },
-                {
-                    mainImg: "/public/img/6.webp",
-                    hoverImg: "/public/img/6b.webp",
-                    brand: "Primark",
-                    description: "SLIM FIT",
-                    priceNow: "14,99€ ",
-                    priceOld: "22,99€ ",
-                },
-                
-            ]
+           products,
         }
     },
 }
@@ -68,8 +19,8 @@ export default {
     <main>
         <div class="main_container container_75">
             <div class="row d-flex">
-                <CardApp :product="product" v-for="product in myCards" />
-
+                <CardApp :product="product" v-for="product in products" />
+                
             </div>
         </div>
     </main>
